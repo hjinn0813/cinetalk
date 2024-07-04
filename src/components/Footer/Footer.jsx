dc3fda091430a76ad0572de5d3d23503b286232f
hjinn0813 <hjc3790@gmail.com>
2024-07-04 14:12:29 +0900

    feat: footer 기본 구조, 디자인 완성

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,42 +2,39 @@
 
 import React from 'react';
 import { Link } from 'react-router-dom';
-import logo from '../../assets/logo.png';
 import '../../styles/Footer/Footer.scss';
 
 export default function Footer() {
   return (
     <div>
       <footer className="footer">
-        <div className="contents" style={{ position: 'relative' }}>
+        <div className="foot-container">
           <div style={{ display: 'inline-block' }}>
-            <div className="footer-logo">
-              <Link to="/" style={{ textDecoration: 'none' }}>
-                {/* 로고 클릭시 이동 */}
-                <div className="logo">
-                  <img src={logo} alt="Logo" />
-                </div>
+            <div className="foot-logo">
+              <Link to="/">
+                <div className="logo">cinetalk</div>
+                {/* 로고 클릭시 홈으로 이동 */}
               </Link>
             </div>
             <div>
-              <div className="title">(주)시네톡</div>
-              <div className="title">대표이사: 조혜진</div>
-              <div className="title">주소: 서울시 서초구 사평대로 52길</div>
-              <div className="title">사업자등록번호: 123-45-67890</div>
-              <div className="title">개인정보 관리 책임자: 홍길동</div>
+              <div className="foot-info">(주)시네톡</div>
+              <div className="foot-info">대표이사: 조혜진</div>
+              <div className="foot-info">주소: 서울시 서초구 사평대로 52길</div>
+              <div className="foot-info">사업자등록번호: 123-45-67890</div>
+              <div className="foot-info">개인정보 관리 책임자: 홍길동</div>
             </div>
             <br />
-            <div className="title">© 2024 Cinetalk Corp.</div>
+            <div className="copyright">© 2024 cinetalk Corp.</div>
           </div>
           <div className="right-text">
-            <div>
-              <div className="title">서비스 이용 약관</div>
-              <div className="title">개인정보 처리 방침</div>
-              <div className="title">회사 소개</div>
+            <div className="right-up">
+              <div className="foot-info">서비스 이용 약관</div>
+              <div className="foot-info">개인정보 처리 방침</div>
+              <div className="foot-info">회사 소개</div>
             </div>
-            <div>
-              <div className="title">FAX: 1234-5678</div>
-              <div className="title">고객센터: 1588-0000 (유료)</div>
+            <div className="right-down">
+              <div className="foot-info">FAX : 1234-5678</div>
+              <div className="foot-info">고객센터 : 1588-0000 (유료)</div>
             </div>
           </div>
         </div>
