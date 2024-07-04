0418f9b2ae12556b46e447685257c5987ebc461d
hjinn0813 <hjc3790@gmail.com>
2024-07-04 12:27:20 +0900

    feat: 헤더 작업 완료

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,47 @@
 // footer
 
 import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../../assets/logo.png';
+import '../../styles/Footer/Footer.scss';
 
 export default function Footer() {
-  return <div>Footer</div>;
+  return (
+    <div>
+      <footer className="footer">
+        <div className="contents" style={{ position: 'relative' }}>
+          <div style={{ display: 'inline-block' }}>
+            <div className="footer-logo">
+              <Link to="/" style={{ textDecoration: 'none' }}>
+                {/* 로고 클릭시 이동 */}
+                <div className="logo">
+                  <img src={logo} alt="Logo" />
+                </div>
+              </Link>
+            </div>
+            <div>
+              <div className="title">(주)시네톡</div>
+              <div className="title">대표이사: 조혜진</div>
+              <div className="title">주소: 서울시 서초구 사평대로 52길</div>
+              <div className="title">사업자등록번호: 123-45-67890</div>
+              <div className="title">개인정보 관리 책임자: 홍길동</div>
+            </div>
+            <br />
+            <div className="title">© 2024 Cinetalk Corp.</div>
+          </div>
+          <div className="right-text">
+            <div>
+              <div className="title">서비스 이용 약관</div>
+              <div className="title">개인정보 처리 방침</div>
+              <div className="title">회사 소개</div>
+            </div>
+            <div>
+              <div className="title">FAX: 1234-5678</div>
+              <div className="title">고객센터: 1588-0000 (유료)</div>
+            </div>
+          </div>
+        </div>
+      </footer>
+    </div>
+  );
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,8 +4,9 @@ import { Link } from 'react-router-dom';
 import logo from '../../assets/logo.png';
 import '../../styles/Header/Header.scss';
 
-// import { ReactComponent as Book } from '../../assets/Header/Book.svg';
+import { ReactComponent as Book } from '../../assets/Header/Book.svg';
 import { ReactComponent as Person } from '../../assets/Header/Person.svg';
+import { ReactComponent as Friends } from '../../assets/Header/Friends.svg';
 
 export default function Header() {
   return (
@@ -13,31 +14,37 @@ export default function Header() {
       <header className="header">
         <div className="container">
           <div className="main-logo">
-            <Link to="/" style={{ textDecoration: 'none' }}>
-              {/* 로고 클릭시 이동 */}
-              <div className="logo">
-                <img src={logo} alt="Logo" />
-                <div className="logo-1">홈</div>
-              </div>
+            <Link to="/">
+              <img src={logo} alt="Logo" className="logo" />
+              {/* 로고 이미지 클릭시 홈으로 이동 */}
             </Link>
           </div>
           <div className="icon-container">
-            <Link to="/MyPage" className="header-link">
-              <Person className="icon" />
-            </Link>
-            <Link to="/MyPage" className="header-link">
-              <span>마이 페이지</span>
-            </Link>
+            <div className="header-btns">
+              <Link to="#" className="header-link">
+                <Book className="icon" />
+              </Link>
+              <Link to="#" className="header-link">
+                <span className="header-txt">New</span>
+              </Link>
+            </div>
+            <div className="header-btns">
+              <Link to="#" className="header-link">
+                <Friends className="icon" />
+              </Link>
+              <Link to="#" className="header-link">
+                <span className="header-txt">Friends</span>
+              </Link>
+            </div>
+            <div className="header-btns">
+              <Link to="/MyPage" className="header-link">
+                <Person className="icon" />
+              </Link>
+              <Link to="/MyPage" className="header-link">
+                <span className="header-txt">My Page</span>
+              </Link>
+            </div>
           </div>
-          {/* <div className="icon-container">
-            <Link to="/Watched" className="header-link">
-              <Book className="icon" />
-            </Link>
-            <Link to="/Watched" className="header-link">
-              <span>라이브러리</span>
-            </Link>
-          </div> */}
-          {/* 수정 요망 */}
         </div>
       </header>
     </div>
