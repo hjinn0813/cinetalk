785881fdcf90ad7da15d480cc7240ddcb9aba69c
hjinn0813 <hjc3790@gmail.com>
2024-07-05 12:23:31 +0900

    feat: 메인페이지 리뷰 카드 상세내역 작업

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,29 +1,21 @@
 import React from 'react';
-import ReviewBox from '../components/Main/ReviewBox';
 import '../styles/Main/Main.scss';
+import ReviewBox from '../components/Main/ReviewBox';
+import Reviews from '../components/Main/Reviews.json';
+import Friends from '../components/Main/Friends.json';
 
 export default function Main() {
-  const reviews = [
-    { id: 1, MovieName: 'a', PostTitle: 'a', author: 'a' },
-    { id: 2, MovieName: 'b', PostTitle: 'b', author: 'b' },
-    { id: 3, MovieName: 'c', PostTitle: 'c', author: 'c' },
-    { id: 4, MovieName: 'd', PostTitle: 'd', author: 'd' },
-    { id: 5, MovieName: 'e', PostTitle: 'e', author: 'e' },
-    { id: 6, MovieName: 'f', PostTitle: 'f', author: 'f' },
-    // 추가 리뷰들
-  ];
-
   return (
     <div>
       <section className="main-container">
         <div className="main-item">
           <div className="new">지금 뜨는 최신 글</div>
           <div className="card-list">
-            {reviews.map((review) => (
+            {Reviews.map((review) => (
               <ReviewBox
                 key={review.id}
-                MovieName={review.MovieName}
-                PostTitle={review.PostTitle}
+                MovieName={review.Movie_Name}
+                PostTitle={review.Post_Title}
                 author={review.author}
               />
             ))}
@@ -32,11 +24,11 @@ export default function Main() {
         <div className="main-item">
           <div className="friends">내 친구는 지금</div>
           <div className="card-list">
-            {reviews.map((review) => (
+            {Friends.map((review) => (
               <ReviewBox
                 key={review.id}
-                MovieName={review.MovieName}
-                PostTitle={review.PostTitle}
+                MovieName={review.Movie_Name}
+                PostTitle={review.Post_Title}
                 author={review.author}
               />
             ))}
