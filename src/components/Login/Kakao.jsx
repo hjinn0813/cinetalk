d7407102df4ebe49291b515b18d294b79adee4df
hjinn0813 <hjc3790@gmail.com>
2024-07-09 15:07:54 +0900

    feat: 소셜로그인 기능 연구중

--- src/components/Login/Kakao.jsx
+++ src/components/Login/Kakao.jsx
@@ -1,15 +1,13 @@
 // 카카오 로그인 버튼
 import React from 'react';
 import '../../styles/Login/Kakao.scss';
+import { KAKAO_AUTH_URL } from '../Login/Oauth';
 
 export default function Kakao() {
-  const KAKAO_api_key = 'process.env.REACT_APP_KAKAO_API'; //REST API KEY
-  const redirect_url = 'http://localhost:3000/auth'; //Redirect URI
-  // oauth 요청 URL
-  const kakaoURL = `https://kauth.kakao.com/oauth/authorize?client_id=${KAKAO_api_key}&redirect_uri=${redirect_url}&response_type=code`;
   const handleLogin = () => {
-    window.location.href = kakaoURL;
+    window.location.href = KAKAO_AUTH_URL;
   };
+
   return (
     <>
       <button className="kakao-login" onClick={handleLogin}>
--- src/components/Login/Naver.jsx
+++ src/components/Login/Naver.jsx
@@ -1,13 +1,9 @@
 // 네이버 로그인 버튼
 import React from 'react';
 import '../../styles/Login/Naver.scss';
+import { NAVER_AUTH_URL } from '../Login/Oauth';
 
 export default function Naver() {
-  const NAVER_api_key = 'process.env.REACT_APP_NAVER_API';
-  const REDIRECT_URI = 'http://localhost:3000/oauth'; // Callback URL
-  const STATE = 'flase';
-  const NAVER_AUTH_URL = `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${NAVER_api_key}&state=${STATE}&redirect_uri=${REDIRECT_URI}`;
-
   const NaverLogin = () => {
     window.location.href = NAVER_AUTH_URL;
   };
