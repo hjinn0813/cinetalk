5d7168ed3ecc9ede995f44274d0be49490d4cc48
hjinn0813 <hjc3790@gmail.com>
2024-07-09 11:11:17 +0900

    feat: 리뷰 작성하기 페이지와 컴포넌트 기본 구조와 디자인 완료

--- src/components/Write/toggle.jsx
+++ src/components/Write/toggle.jsx
@@ -1,6 +1,19 @@
 // 스포일러 포함, 비공개 여부 - 토글 스위치
-import React from 'react';
+import React, { useState } from 'react';
+import '../../styles/Write/Toggle.scss'; // 스타일을 위한 CSS 파일
 
-export default function toggle() {
-  return <div>toggle</div>;
-}
+const Toggle = () => {
+  const [isOn, setIsOn] = useState(false);
+
+  const toggleSwitch = () => {
+    setIsOn(!isOn);
+  };
+
+  return (
+    <div className={`toggle-switch ${isOn ? 'on' : ''}`} onClick={toggleSwitch}>
+      <div className="switch"></div>
+    </div>
+  );
+};
+
+export default Toggle;
--- src/pages/Write.jsx
+++ src/pages/Write.jsx
@@ -1,7 +1,55 @@
 // 리뷰 작성하기 페이지
 
 import React from 'react';
+import Toggle from '../components/Write/toggle';
+import '../styles/Write/Write.scss';
 
-export default function write() {
-  return <div>write</div>;
+export default function Write() {
+  return (
+    <div className="write-container">
+      <div className="w-title">리뷰 작성하기</div>
+      <form className="write-input">
+        <div className="title-area">
+          <label>
+            <div>리뷰제목</div>
+            <input type="text" className="inputarea ipt-title" />
+          </label>
+        </div>
+        <div className="body-area">
+          <label>
+            <div>리뷰본문</div>
+            <textarea rows="5" cols="80" className="inputarea ipt-body" />
+          </label>
+        </div>
+        <div className="date-area">
+          <label>
+            <div>
+              관람일&nbsp;&nbsp;
+              <input type="date" className="inputarea ipt-date" />
+            </div>
+          </label>
+        </div>
+        <div className="url-area">
+          <label>
+            <div>URL 추가</div>
+            <input type="text" className="inputarea ipt-url" />
+          </label>
+        </div>
+
+        <div className="write-toggle">
+          <div className="tg-btn">
+            스포일러 포함&nbsp;&nbsp;
+            <Toggle />
+          </div>
+          <div className="tg-btn">
+            비공개 여부&nbsp;&nbsp;
+            <Toggle />
+          </div>
+        </div>
+        <div className="apply">
+          <button className="apply-btn">확인</button>
+        </div>
+      </form>
+    </div>
+  );
 }
