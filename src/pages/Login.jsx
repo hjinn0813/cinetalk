6d4de42cf7adfcf0c0019e778d3a1c8ae4b4274e
hjinn0813 <hjc3790@gmail.com>
2024-07-10 15:59:27 +0900

    feat: login page input 추가

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -9,6 +9,17 @@ export default function Login() {
   return (
     <div className="lg-wrap">
       <div className="lg-title">Login</div>
+      <div className="lg-input">
+        <label>
+          <input type="text" id="userId" placeholder="아이디" />
+        </label>
+        <label>
+          <input type="password" id="password" placeholder="비밀번호" />
+        </label>
+        <div className="lg-apply">
+          <button className="lg-apply-btn">확인</button>
+        </div>
+      </div>
       <div className="lg-btns">
         <Kakao />
         <Naver />
