e5fdaf9bb87a36b96ab224255a95211fc493c5cd
hjinn0813 <hjc3790@gmail.com>
2024-07-16 13:13:45 +0900

    feat: 리뷰 상세보기 좋아요, 공유하기 버튼 기능 구현

--- src/components/Review/ReadPosts.jsx
+++ src/components/Review/ReadPosts.jsx
@@ -1,9 +1,34 @@
 // 리뷰 상세보기 컴포넌트 구조
-import React from 'react';
+import React, { useState } from 'react';
 import '../../styles/Review/ReadPosts.scss';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import ShareIcon from '@mui/icons-material/Share';
 
 export default function ReadPosts({ poster, title, date, body }) {
   const posterImage = require(`../../assets/Watched/${poster}.jpg`);
+  const [liked, setLiked] = useState(false);
+  const [likeCount, setLikeCount] = useState(0);
+
+  // 좋아요 버튼 클릭 이벤트
+  const handleLikeClick = () => {
+    if (liked) {
+      setLikeCount(likeCount - 1);
+    } else {
+      setLikeCount(likeCount + 1);
+    }
+    setLiked(!liked);
+  };
+
+  // 공유하기 버튼 클릭시 주소 복사
+  const handleShareClick = async () => {
+    try {
+      await navigator.clipboard.writeText(window.location.href);
+      alert('URL이 복사되었습니다!');
+    } catch (err) {
+      alert('URL 복사에 실패했습니다.');
+    }
+  };
 
   return (
     <div className="r-container">
@@ -16,6 +41,19 @@ export default function ReadPosts({ poster, title, date, body }) {
           </div>
         </div>
         <div className="r-body">{body}</div>
+        <div className="r-actions">
+          <div
+            className={`r-like-button ${liked ? 'liked' : ''}`}
+            onClick={handleLikeClick}
+          >
+            {liked ? <FavoriteIcon /> : <FavoriteBorderIcon />} 좋아요{' '}
+            {likeCount}
+          </div>
+          <div className="r-share-button" onClick={handleShareClick}>
+            <ShareIcon />
+            공유하기
+          </div>
+        </div>
       </div>
     </div>
   );
