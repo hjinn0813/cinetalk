a5ebcbc1f9f0bad1cbdd9c9843e9898e07fd7323
hjinn0813 <hjc3790@gmail.com>
2024-07-16 17:56:03 +0900

    fix: 부모 태그 수정

--- src/components/Main/ReviewBox.jsx
+++ src/components/Main/ReviewBox.jsx
@@ -4,12 +4,12 @@ import '../../styles/Main/ReviewBox.scss';
 
 export default function ReviewBox({ MovieName, PostTitle, author }) {
   return (
-    <div>
+    <>
       <div className="main-box">
         <div className="main-movie-title">{MovieName}</div>
         <div className="main-post-title">{PostTitle}</div>
         <div className="main-author">{author}</div>
       </div>
-    </div>
+    </>
   );
 }
