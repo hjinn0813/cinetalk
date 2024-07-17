0e1e112ba13e31c4d3b6df3c8d4412739b90357f
hjinn0813 <hjc3790@gmail.com>
2024-07-17 16:17:10 +0900

    fix: 리뷰 박스 크기 비율 설정, 최상위태그 수정

--- src/components/Main/ReviewBox.jsx
+++ src/components/Main/ReviewBox.jsx
@@ -4,12 +4,10 @@ import '../../styles/Main/ReviewBox.scss';
 
 export default function ReviewBox({ MovieName, PostTitle, author }) {
   return (
-    <>
-      <div className="main-box">
-        <div className="main-movie-title">{MovieName}</div>
-        <div className="main-post-title">{PostTitle}</div>
-        <div className="main-author">{author}</div>
-      </div>
-    </>
+    <div className="main-box">
+      <div className="main-movie-title">{MovieName}</div>
+      <div className="main-post-title">{PostTitle}</div>
+      <div className="main-author">{author}</div>
+    </div>
   );
 }
