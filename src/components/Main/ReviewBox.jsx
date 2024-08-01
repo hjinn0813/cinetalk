3fc6dc3ec7a03c22ab423600d96e9fd839228817
hjinn0813 <hjc3790@gmail.com>
2024-08-01 11:24:29 +0900

    fix: 리뷰 내용 수정, 멘트 추가, 신호등 추가

--- src/components/Main/ReviewBox.jsx
+++ src/components/Main/ReviewBox.jsx
@@ -2,12 +2,20 @@
 import React from 'react';
 import '../../styles/Main/ReviewBox.scss';
 
-export default function ReviewBox({ MovieName, PostTitle, author }) {
+export default function ReviewBox({
+  MovieName,
+  PostTitle,
+  trafficLight,
+  author,
+}) {
   return (
     <div className="main-box">
       <div className="main-movie-title">{MovieName}</div>
       <div className="main-post-title">{PostTitle}</div>
-      <div className="main-author">{author}</div>
+      <div className="main-author">
+        <div className={`traffic-light ${trafficLight}`}></div>
+        <div>{author} 님의 리뷰</div>
+      </div>
     </div>
   );
 }
