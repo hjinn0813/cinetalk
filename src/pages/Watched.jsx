1221768f05293654b45358e7a5676bebc92aae69
hjinn0813 <hjc3790@gmail.com>
2024-08-08 13:40:47 +0900

    feat: 모바일에서만 swiper 슬라이드 설정

--- src/pages/Watched.jsx
+++ src/pages/Watched.jsx
@@ -1,12 +1,29 @@
 // 라이브러리 (개인의 리뷰 목록) 페이지
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import WatchedPoster from '../components/Watched/WatchedPoster';
 import '../styles/Watched/Watched.scss';
 
+// Swiper
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+
 export default function Watched() {
   const posts = useSelector((state) => state.posts.posts);
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768);
+    };
+
+    window.addEventListener('resize', handleResize); // 크기 변경시 함수 호출
+    return () => window.removeEventListener('resize', handleResize);
+    // 필요 없는 이벤트 리스너 정리
+  }, []);
 
   return (
     <div className="library-container">
@@ -14,11 +31,29 @@ export default function Watched() {
       <div className="library-list">
         {posts.length === 0 ? (
           <div>해당 리뷰가 없습니다!😭</div>
+        ) : isMobile ? (
+          <Swiper
+            spaceBetween={10}
+            slidesPerView={1}
+            navigation={{ clickable: true }}
+            modules={[Navigation]}
+          >
+            {posts.map((card) => (
+              <SwiperSlide key={card.id}>
+                <Link to={`/Review/${card.id}`} className="movingbtn">
+                  <WatchedPoster
+                    poster={require(`../assets/Watched/${card.poster}.jpg`)}
+                    title={card.title}
+                    date={card.date}
+                  />
+                </Link>
+              </SwiperSlide>
+            ))}
+          </Swiper>
         ) : (
           posts.map((card) => (
             <Link key={card.id} to={`/Review/${card.id}`} className="movingbtn">
               <WatchedPoster
-                key={card.id}
                 poster={require(`../assets/Watched/${card.poster}.jpg`)}
                 title={card.title}
                 date={card.date}
